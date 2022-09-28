7cdedcba297a8e4cb809800148b12b3fa05cba05
robotroot2022 <114375318+robotroot2022@users.noreply.github.com>
2022-09-28 14:30:14 -0500

    Delete RobotRoot directory

--- RobotRoot/src/main/java/com/RobotRoot/Repositories/RepositorioEmpleado.java
+++
@@ -1,9 +0,0 @@
-package com.RobotRoot.Repositories;
-
-import com.RobotRoot.Entities.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioEmpleado extends JpaRepository<Empleado, Long> {
-}
